f8f24413e9f61f1240f3351d28a065d1e9eb3e8f
Nam Phung Thanh <spnether1411@gmail.com>
2022-05-12 15:36:53 +0700

    Add auth models

---
+++ pkg/models/auth.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	uuid "github.com/satori/go.uuid"
+)
+
+type RefreshTokenInput struct {
+        RefreshToken string `form:"refresh_token" json:"refresh_token" binding:"required"`
+}
+
+type Tokens struct {
+        AccessToken string `json:"access_token"`
+        RefreshToken string `json:"refresh_token"`
+}
+
+type UserLogin struct {
+        Email string `form:"email" json:"email" binding:"required,email"`
+        Password string `form:"password" json:"password" binding:"required,min=3,max=50"`
+}
+
+type TokenMeta struct {
+        AccessToken string
+        RefreshToken string
+        AccessUUID uuid.UUID
+        RefreshUUID uuid.UUID
+        AccessExpires int64
+        RefreshExpires int64
+}
+
+type AccessMeta struct {
+        AccessUUID uuid.UUID
+        UserID int64
+}
