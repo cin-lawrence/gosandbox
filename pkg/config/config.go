79c0b7b9c400399043651a6a4b7632c676789ac4
Nam Phung Thanh <spnether1411@gmail.com>
2022-05-12 15:35:13 +0700

    Update api config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,12 +8,26 @@ type config struct {
 	BrokerURI      string `mapstructure:"BROKER_URI"`
 	DatabaseURI    string `mapstructure:"DATABASE_URI"`
 	Port           int    `mapstructure:"PORT"`
+
+        AccessTokenExpirationTime int64 `mapstructure:"ACCESS_TOKEN_EXPIRATION_TIME"`
+        RefreshTokenExpirationTime int64 `mapstructure:"REFRESH_TOKEN_EXPIRATION_TIME"`
+        AccessTokenSecret string `mapstructure:"ACCESS_TOKEN_SECRET"`
+        RefreshTokenSecret string `mapstructure:"SECRET_TOKEN_SECRET"`
+
+        RedisAddress string `mapstructure:"REDIS_ADDRESS"`
+        RedisPassword string `mapstructure:"REDIS_PASSWORD"`
+        RedisDB int `mapstructure:"REDIS_DB"`
+
 	CeleryTaskName string `mapstructure:"CELERY_TASK_NAME"`
 }
 
 var Config config = LoadConfig()
 
 func setDefaults(v *viper.Viper) {
+        v.SetDefault("ACCESS_TOKEN_EXPIRATION_TIME", 15 * 60)
+        v.SetDefault("REFRESH_TOKEN_EXPIRATION_TIME", 7 * 24 * 60 * 60)
+        v.SetDefault("ACCESS_TOKEN_SECRET", "paragona")
+        v.SetDefault("REFRESH_TOKEN_SECRET", "paragonf")
 	v.SetDefault("CELERY_TASK_NAME", "fibonacci")
 }
 
