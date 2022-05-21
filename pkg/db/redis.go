a6b171774f5aafc2282bf38481c93f2fdee18a2a
Nam Phung Thanh <spnether1411@gmail.com>
2022-05-21 16:48:30 +0700

    Adapt to test mode

--- pkg/db/redis.go
+++ pkg/db/redis.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/cin-lawrence/gosandbox/pkg/config"
 
+	miniredis "github.com/alicebob/miniredis/v2"
 	redis "github.com/go-redis/redis/v8"
 )
 
@@ -12,6 +13,15 @@ var RedisClient *redis.Client = InitRedis()
 var RedisContext context.Context = context.TODO()
 
 func InitRedis() *redis.Client {
+	if config.Config.Test {
+		mr, err := miniredis.Run()
+		if err != nil {
+			panic(err)
+		}
+		return redis.NewClient(&redis.Options{
+			Addr: mr.Addr(),
+		})
+	}
 	return redis.NewClient(&redis.Options{
 		Addr:     config.Config.RedisAddress,
 		Password: config.Config.RedisPassword,
